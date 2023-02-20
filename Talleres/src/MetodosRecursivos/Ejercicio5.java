5fb0079cc7e1aa0246a16a84798486f99f92992b
Nicolás Jurado <99767358+NicholashiG@users.noreply.github.com>
2023-02-19 20:48:18 -0500

    Se actualiza el ejercicio 5 de recursividad
    Ya sirve, todo joya, solo eran unos errores en los momentos donde se hacían las comparaciones...

--- Talleres/src/MetodosRecursivos/Ejercicio5.java
+++ Talleres/src/MetodosRecursivos/Ejercicio5.java
@@ -16,30 +16,36 @@ public class Ejercicio5 {
      */
     public static void main(String[] args) {
 
-        int[] arreglo = {1, 6, 0, 3, 6, 8, 2};
-        int[] arrMayorMenor = {0, 0};
-        arrMayorMenor = encontrarMayorMenorArreglo(arreglo, 0, arrMayorMenor);
-        System.out.println(arrMayorMenor[0]);
-        System.out.println(arrMayorMenor[1]);
+        int[] arreglo = {-2, 6, 10, 3, 6, 8, 22};
+        int[] arrMayorMenor = {arreglo[0], arreglo[0]};
+        arrMayorMenor = encontrarMayMen(arreglo, 0, arrMayorMenor);
+        System.out.println("El menor del arreglo es: " + arrMayorMenor[0]);
+        System.out.println("El mayor del arreglo es: " + arrMayorMenor[1]);
 
     }
-    public static int[] encontrarMayorMenorArreglo(int[] arreglo, int i, int[] arrMayMen){
-        int[] arregloMayorMenor=arrMayMen;
-        if (i<arreglo.length-1){
-            arregloMayorMenor = encontrarMayorMenorArreglo(arreglo, i+1, arregloMayorMenor);
-            if (arreglo[i]>arregloMayorMenor[0]){
-                arregloMayorMenor[0]=arreglo[i];
+
+    public static int[] encontrarMayMen(int[] arr, int i, int[] arrMayMen) {
+        int[] arregloMayorMenor = arrMayMen;
+        if (arr != null) {
+            if (arr.length != 0) {
+                if (i <= arr.length - 1) {
+                    if (arr[i] > arrMayMen[1]) { // Comparamos si el arreglo en la posición i es mayor al arregloMayMen en la posición 1, es decir en la del mayor
+                        arregloMayorMenor[1] = arr[i]; // Si es así, se asigna en el arregloMayMen ese valor
+                        return (encontrarMayMen(arr, i + 1, arregloMayorMenor)); // Aquí retornamos la función para que revise el siguiente en el arreglo
+                    } else { // Si la condición de la línea 32 no se cumple, saltamos aquí
+                        if (arr[i] < arrMayMen[0]) { // Comparamos si el arreglo en la posición i es menor al arregloMayMen en la posición 0, es decir en la del menor
+                            arregloMayorMenor[0] = arr[i]; // Si es así, se asigna en el arregloMayMen ese valor
+                            return (encontrarMayMen(arr, i + 1, arregloMayorMenor)); // Aquí retornamos la función para que revise el siguiente en el arreglo
+                        } else {
+                            return encontrarMayMen(arr, i + 1, arregloMayorMenor); // Si no es ni mayor ni menor, se revisa el siguiente
+                        }
+
+                    }
+                }
             }
-            if (arreglo[i]<arregloMayorMenor[1]){
-                arregloMayorMenor[1]=arreglo[i];
-            }
-            return encontrarMayorMenorArreglo(arreglo, i+1, arregloMayorMenor);
         }
-        else {
-            arregloMayorMenor[0]=arreglo[i];
-            arregloMayorMenor[1]=arreglo[i];
-            return arregloMayorMenor;
-        }
+        return arregloMayorMenor; // Al final de esto, retornamos el arreglo con los valores de menor y mayor en la posición 0 y 1 respectivamente
     }
+
 
 }
